f543f9b3d96ff7b29a7f556958dfc808d1631d6b
pattisonh72-blip <pattisonh72@gmail.com>
2026-06-22 12:44:11 +0530

    Update oneword.py

--- modules/oneword.py
+++ modules/oneword.py
@@ -7,7 +7,7 @@ import asyncio
 from utils.misc import modules_help, prefix
 
 TXT_FILE = "database/oneword.txt"
-active_tasks = set()
+active_tasks = {}
 
 def load_txt_list():
     if not os.path.exists(TXT_FILE):
@@ -20,40 +20,50 @@ def load_txt_list():
 
 @Client.on_message(filters.command("ow", prefix) & filters.me)
 async def activate_loop(client: Client, message: Message):
+    chat_id = message.chat.id
+    
+    if chat_id in active_tasks:
+        await message.delete()
+        return
+
     text_list = load_txt_list()
     if not text_list:
         await message.delete()
         return
         
-    task_id = asyncio.current_task()
-    active_tasks.add(task_id)
-    
     await message.delete()
     reply_to_id = message.reply_to_message.id if message.reply_to_message else None
+    
+    active_tasks[chat_id] = True
 
     for item in text_list:
-        if task_id not in active_tasks:
+        if chat_id not in active_tasks:
             break
+            
         try:
             if reply_to_id:
-                await client.send_message(message.chat.id, item, reply_to_message_id=reply_to_id)
+                await client.send_message(chat_id, item, reply_to_message_id=reply_to_id)
             else:
-                await client.send_message(message.chat.id, item)
+                await client.send_message(chat_id, item)
             
-            await asyncio.sleep(0.15)
+            # Extreme Fast Speed
+            await asyncio.sleep(0.1)
             
         except FloodWait as e:
+            # FloodWait aane par bot bina ruke utne seconds wait karke khud aage badhega
             await asyncio.sleep(e.value + 1)
+            
         except Exception:
-            pass
+            await asyncio.sleep(0.1)
 
-    if task_id in active_tasks:
-        active_tasks.remove(task_id)
+    active_tasks.pop(chat_id, None)
 
 @Client.on_message(filters.command("owstop", prefix) & filters.me)
 async def stop_loop(client: Client, message: Message):
-    global active_tasks
-    active_tasks.clear() 
+    chat_id = message.chat.id
+    if chat_id in active_tasks:
+        active_tasks.pop(chat_id, None)
+    
     await message.delete()
 
 modules_help["oneword"] = {
